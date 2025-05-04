ecd0b10133892bfc5dcefddf073e75831b2bc50d
21B1NUM0435 <21b1num0435@stud.num.edu.mn>
2025-05-05 00:55:04 +0800

    Authentication done.

--- src/components/navbar/CustomNavBar.js
+++ src/components/navbar/CustomNavBar.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Layout, Menu, Avatar, Dropdown, Space, Badge } from "antd";
-import { BellOutlined, DownOutlined, MenuOutlined } from "@ant-design/icons";
+import { Layout, Menu, Avatar, Dropdown, Space } from "antd";
+import { DownOutlined, MenuOutlined } from "@ant-design/icons";
 
-import NotificationBell from "../notifications/NotificationBell";
 
 
 const { Header } = Layout;
@@ -99,28 +98,6 @@ const CustomNavBar = ({ user, setUser, logoutFunction, onClick }) => {
             </div>
           ))}
         </div>
-
-
-        {/* <NotificationBell /> -aar tur soliv */}
-        {/* <div
-          style={{
-            backgroundColor: "white",
-            borderRadius: "50%",
-            padding: "7px",
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <Badge offset={[10, 0]}>
-            <BellOutlined
-              style={{ fontSize: "20px", cursor: "pointer", color: "gray" }}
-            />
-          </Badge>
-        </div> */}
-
-        <NotificationBell />
 
         <div
           style={{
